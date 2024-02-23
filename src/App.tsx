30e9bdb76d760313a43ff28913a779c46e4a930d
Tkachev Maksim <maks93makula@gmail.com>
2024-02-23 18:49:08 +0600

    Создан App.tsx со всеми компонентами и версткой, App.css написана вся стилизация сайта

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './components/store/store';
+import KeyBoard from './components/Keyboard/Keyboard';
+import Screen from './components/Screen/Screen';
+import Result from './components/Result/Result';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+    return (
+        <div className='main-block-key'>
+            <div className='backrgound-key'>
+        <Provider store={store}>
+            <div className="main-keyboard">
+                <div className='screen-display'>
+                    <Screen />
+                </div>
+                <Result />
+                <KeyBoard />
+            </div>
+        </Provider>
+            </div>
+        </div>
+    );
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
